74af91260885b5731671bed754adc68722920ee2
Aneunsu <158244832+Aneunsu@users.noreply.github.com>
2024-01-31 12:48:41 +0900

    Add files via upload

---
+++ day01/var.cpp
@@ -0,0 +1,19 @@
+//var
+#include<stdio.h>
+
+int main() {
+
+	// 30 + 20: 사람특) 연산+기억 동시
+	// 30 + 20: 컴퓨터특) 연산 <-> 기억 따로
+
+
+	// 정수/실수/... 변수
+	// int - integer[독일어 정수]
+	int num = 100;
+	int son = 7;
+
+	printf("num의 값은: %d", num);
+	printf("num+son=%d", num + son);
+
+	return 0;
+}
